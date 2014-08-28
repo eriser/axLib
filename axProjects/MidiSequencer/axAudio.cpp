4cb06387069bd67114a17ad75fc0e54ccf2f5fe4
alexarse <arsemusic@live.com>
2014-08-27 23:36:21 -0400

    Fix audio output for Windows.

--- axProjects/MidiSequencer/axAudio.cpp
+++ axProjects/MidiSequencer/axAudio.cpp
@@ -177,7 +177,7 @@ Audio::Audio()
 	}
 
 
-	outputParameters.device = 1;//;//Pa_GetDefaultOutputDevice(); //1
+	outputParameters.device = 4;//1;//;//Pa_GetDefaultOutputDevice(); //1
 	// cout << "OUTPU : " << outputParameters.device.name << endl;
 	if(outputParameters.device == paNoDevice)
 	{
