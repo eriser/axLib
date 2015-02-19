b5000d49e212a584a672dc3cfffca1e800a2fb6a
alarsa <alexandre.arsenault@polymtl.ca>
2015-02-18 19:40:10 -0500

    Add comment to axFrameBuffer.

--- source/axFrameBuffer.cpp
+++ source/axFrameBuffer.cpp
@@ -1,10 +1,24 @@
-//
-//  axGraphicInterface.cpp
-//  axLib
-//
-//  Created by Alexandre Arsenault on 2015-02-17.
-//  Copyright (c) 2015 Alexandre Arsenault. All rights reserved.
-//
+/*******************************************************************************
+ * Copyright (c) 2015 Alexandre Arsenault.
+ *
+ * This file is part of axLibrary.
+ *
+ * axLibrary is free or commercial software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 or any later version of the
+ * License or use a commercial axLibrary License.
+ *
+ * axLibrary is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with axLibrary. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * To release a closed-source product which uses axLibrary, commercial
+ * licenses are available, email alx.arsenault@gmail.com for more information.
+ ******************************************************************************/
 
 #include "axFrameBuffer.h"
 #include "axMath.h"
